e7abf6cd422a1461f6ca257dd4c5da8dacca9e67
WIN-H774NMII882\Windows 8.1 Ultimate <diemxuan2812@yahoo.com.vn>
2017-12-09 18:39:10 +0700

    example 11 structural directive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,4 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  showLineif = true;
+ public abc = 'blue';
+ colors: string[] = ['red', 'blue', 'yellow'];
 }
